90dcccedd75573ff6259a5aa5dee440a5545bc03
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2023-06-07 01:52:33 +0200

    authentication + loginpage + where user can get all cases assigned to them

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,22 @@
+import { Routes, Route, Link, NavLink} from 'react-router-dom';
+import { useAuth } from '../pages/auth/auth';
+
 const Navbar = () => {
+    const auth = useAuth()
     return ( 
         <nav className="navbar">
             <h1>Safe Space</h1>
             <div className="links">
-                <a href="/">Home</a>
-                {/* <a href="/">Contacts</a> */}
-                {/* <a href="/">Reported Cases</a> */}
-                <a href="/">About</a>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      <NavLink to="/profile">profile</NavLink>
+      {!auth.user && (
+        <NavLink to='/login' >
+          Login
+        </NavLink>
+      )}
+
+    
             </div>
         </nav>
      );
